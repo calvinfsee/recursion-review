0f859cf868e13b7d40e0d524721a022f764a258e
Calvin See <calvinfrancissee@gmail.com>
2022-02-22 14:00:02 -0800

    Implemented a function that stringifies base cases

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,33 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  //Input: object
+  //Output: string
+  //Edge cases: null (classified as object), arrays (classified as an object), undefined/functions (invalid)
+  //Plan:
+
+  var stringified = '';
+
+  //HELPER FUNCTIONS
+
+  //Stringifies Base Cases: strings, numbers, booleans
+  var stringify = function () {
+    if (obj === undefined || typeof obj === typeof function(){}) {
+      return '';
+    }
+    if (typeof obj === typeof '') {
+      return '"' + obj + '"';
+    } else {
+      return '' + obj; //numbers and booleans
+    }
+  };
+
+  //MAIN
+  if (typeof obj === 'object') { //Accepts NULL, objects, and arrays
+
+  } else { //Base Cases
+    stringified += stringify(obj);
+  }
+
+  return stringified;
 };
